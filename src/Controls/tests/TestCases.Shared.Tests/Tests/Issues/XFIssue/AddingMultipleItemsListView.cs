4997b5b35e7fbc43012497aa6f68014b2e2c09f0
Anandhan Rajagopal <97146406+anandhan-rajagopal@users.noreply.github.com>
2024-11-12 21:08:57 +0530

    Revert "modified the changes in AddingMultipleItemsListView.cs and Issue530.cs"
    This reverts commit a0de6633a062e9c4ea0433e9f4076004b4e1cd84.

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/AddingMultipleItemsListView.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/AddingMultipleItemsListView.cs
@@ -17,17 +17,7 @@ public class AddingMultipleItemsListView : _IssuesUITest
 	public void AddingMultipleListViewTests1AllElementsPresent()
 	{
 		App.WaitForElement("Big Job");
-		App.FindElement("Big Job");
-		App.FindElement("Smaller Job");
-		App.FindElement("Add On Job");
-		App.FindElement("Add One");
-		App.FindElement("Add Two");
-		App.FindElement("3672");
-		App.FindElement("6289");
-		App.FindElement("3672-41");
-		App.FindElement("2");
-		App.FindElement("2");
-		App.FindElement("23");
+		App.Screenshot("All elements are present");
 	}
 
 	[Test]
@@ -36,14 +26,17 @@ public class AddingMultipleItemsListView : _IssuesUITest
 		App.Tap("Add One");
 
 		App.WaitForElement("1234");
+		App.Screenshot("One more element exists");
 	}
 
 	[Test]
 	public void AddingMultipleListViewTests3AddTwoElementToList()
 	{
+		App.Screenshot("Click 'Add Two'");
 		App.Tap("Add Two");
 
 		App.WaitForElement("9999");
-		App.FindElement("8888");
+		App.WaitForElement("8888");
+		App.Screenshot("Two more element exist");
 	}
 }
