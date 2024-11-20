d607d525bbca8b265619ed3697bb5038e1c1e861
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2024-11-20 12:19:31 +0530

    Migrate the Flyout Issues into Appium

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue6784.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue6784.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_CATALYST // null reference exception 
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -13,42 +14,57 @@ public class Issue6784 : _IssuesUITest
 
 	public override string Issue => "ShellItem.CurrentItem is not set when selecting shell section aggregated in more tab";
 
-	//[Test]
-	//public void CurrentItemIsSetWhenSelectingShellSectionAggregatedInMoreTab()
-	//{
-	//	App.WaitForElement(x => x.Class("UITabBarButton").Marked("More"));
-	//	App.Tap(x => x.Class("UITabBarButton").Marked("More"));
+	[Test]
+	public void CurrentItemIsSetWhenSelectingShellSectionAggregatedInMoreTab()
+	{
+#if ANDROID || IOS
+		App.WaitForElement("More");
+		App.Tap("More");
+		App.WaitForElement("Tab 5");
+		App.Tap("Tab 5");
+		App.WaitForElement("Success");
+#else
+		App.WaitForElement("Tab 5");
+		App.Tap("Tab 5");
+		App.WaitForElement("Success");
+#endif
+	}
 
-	//	App.WaitForElement(x => x.Class("UITableViewCell").Text("Tab 5"));
-	//	App.Tap(x => x.Class("UITableViewCell").Text("Tab 5"));
+	[Test]
+	public void MoreControllerOpensOnFirstClick()
+	{
+#if ANDROID || IOS
+		App.WaitForElement("More");
+		App.Tap("More");
+		App.WaitForElement("Tab 5");
+		App.Tap("Tab 5");
+		App.Tap("Tab 4");
+		App.WaitForElement("Tab 4");
+		App.Tap("More");
+		App.WaitForElement("Tab 6");
+#else
+		App.WaitForElement("Tab 5");
+		App.Tap("Tab 5");
+		App.Tap("Tab 4");
+		App.WaitForElement("Tab 4");
+		App.WaitForElement("Tab 6");
+#endif
 
-	//	App.WaitForElement(x => x.Text("Success"));
-	//}
+	}
 
-	//[Test]
-	//public void MoreControllerOpensOnFirstClick()
-	//{
-	//	App.WaitForElement(x => x.Class("UITabBarButton").Marked("More"));
-	//	App.Tap(x => x.Class("UITabBarButton").Marked("More"));
+	[Test]
+	public void MoreControllerDoesNotShowEditButton()
+	{
+#if ANDROID || IOS
+		App.WaitForElement("More");
+		App.Tap("More");
+		App.WaitForElement("Tab 5");
+		Assert.That(App.FindElement("Edit"), Is.Null);
+#else
+		App.WaitForElement("Tab 5");
+		Assert.That(App.FindElement("Edit"), Is.Null);
+#endif
 
-	//	App.WaitForElement(x => x.Class("UITableViewCell").Text("Tab 5"));
-	//	App.Tap(x => x.Class("UITableViewCell").Text("Tab 5"));
-
-	//	App.Tap(x => x.Class("UITabBarButton").Marked("Tab 4"));
-	//	App.WaitForElement("Tab 4 Content");
-
-	//	App.Tap(x => x.Class("UITabBarButton").Marked("More"));
-	//	App.WaitForElement(x => x.Class("UITableViewCell").Text("Tab 6"));
-	//}
-
-	//[Test]
-	//public void MoreControllerDoesNotShowEditButton()
-	//{
-	//	App.WaitForElement(x => x.Class("UITabBarButton").Marked("More"));
-	//	App.Tap(x => x.Class("UITabBarButton").Marked("More"));
-
-	//	App.WaitForElement(x => x.Class("UITableViewCell").Text("Tab 5"));
-
-	//	Assert.AreEqual(App.Query(x => x.Marked("Edit")).Count(), 0);
-	//}
+	}
 }
+#endif
--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue7856.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue7856.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_WINDOWS && TEST_FAILS_ON_CATALYST // text doesnot override in windows and null reference excepion
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -12,18 +13,15 @@ public class Issue7856 : _IssuesUITest
 
 	public override string Issue => "[Bug]  Shell BackButtonBehaviour TextOverride breaks back";
 
-	//[Test]
-	//[Category(UITestCategories.Shell)]
-	//public void BackButtonBehaviorTest()
-	//{
-	//	App.Tap(x => x.Text("Tap to Navigate To the Page With BackButtonBehavior"));
-
-	//	App.WaitForElement(x => x.Text("Navigate again"));
-
-	//	App.Tap(x => x.Text("Navigate again"));
-
-	//	App.WaitForElement(x => x.Text("Test"));
-
-	//	App.Tap(x => x.Text("Test"));
-	//}
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void BackButtonBehaviorTest()
+	{
+		App.Tap("Tap to Navigate To the Page With BackButtonBehavior");
+		App.WaitForElement("Navigate again");
+		App.Tap("Navigate again");
+		App.WaitForElement("Test");
+		App.Tap("Test");
+	}
 }
+#endif
