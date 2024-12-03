f97b5633b6849f4a579babe399b265d45cbb81bc
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2024-12-03 13:27:43 +0530

    migrate uitests

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla41153.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Bugzilla/Bugzilla41153.cs
@@ -12,19 +12,19 @@ public class Bugzilla41153 : _IssuesUITest
 
 	public override string Issue => "jobject must not be IntPtr.Zero with TabbedPage and ToolbarItems";
 
-	// [FailsOnAndroidWhenRunningOnXamarinUITest("Times out on WaitForElement(On Tab 1)")]
-	// [Test]
-	// [Category(UITestCategories.TabbedPage)]
-	// public void Bugzilla41153Test()
-	// {
-	// 	App.WaitForElement("On Tab 1");
-	// 	App.Tap("Tab 2");
-	// 	App.Tap("Tab 3");
-	// 	App.WaitForElement("On Tab 3");
-	// 	App.Tap("Tab 1");
-	// 	App.WaitForElement("On Tab 1");
-	// 	App.Tap("Toolbar Item");
 
-	// 	App.WaitForTextToBePresentInElement("Toolbar Item", "Success");
-	// }
+	[Test]
+	[Category(UITestCategories.TabbedPage)]
+	public void Bugzilla41153Test()
+	{
+		App.WaitForElement("On Tab 1");
+		App.Tap("Tab 2");
+		App.Tap("Tab 3");
+		App.WaitForElement("On Tab 3");
+		App.Tap("Tab 1");
+		App.WaitForElement("On Tab 1");
+		App.Tap("Toolbar Item");
+
+		App.WaitForTextToBePresentInElement("Toolbar Item", "Success");
+	}
 }
