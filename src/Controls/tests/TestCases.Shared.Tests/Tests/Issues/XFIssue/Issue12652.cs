647bc097e9a84acdc9c3ced9bd4151582383724e
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2024-12-05 05:01:30 -0800

    Migrated the Issues 12652, 5159, 8741

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue12652.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue12652.cs
@@ -1,34 +1,51 @@
 ﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
-
+ 
 namespace Microsoft.Maui.TestCases.Tests.Issues;
-
+ 
 public class Issue12652 : _IssuesUITest
 {
+#if ANDROID
+    const string Top3 = "TOP 3";
+#else
+	const string Top3 = "Top 3";
+#endif
 	public Issue12652(TestDevice testDevice) : base(testDevice)
 	{
 	}
-
+ 
 	public override string Issue => "[Bug] NullReferenceException in the Shell on UWP when navigating back to Shell Section with multiple content items";
+ 
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void NavigatingBackToAlreadySelectedTopTabDoesntCrash()
+	{
+		TopTab(Top3);
+		App.WaitForElement("TopTabPage3");
+		App.Tap("Main 2");
+		App.WaitForElement("TopTabPage2");
+		App.Tap("Main 1");
+		App.WaitForElement("TopTabPage3");
+#if WINDOWS
+        App.Tap("Main 2");
+#endif
+		App.Tap("Main 2");
+		App.WaitForElement("TopTabPage2");
+		App.Tap("Main 1");
+		App.WaitForElement("TopTabPage3");
+	}
 
-	// [Test]
-	// [Category(UITestCategories.Shell)]
-	// public void NavigatingBackToAlreadySelectedTopTabDoesntCrash()
-	// {
-	// 	var location = App.WaitForElement("Top 3")[0];
-	// 	App.TapCoordinates(location.Rect.CenterX, location.Rect.CenterY);
-	// 	App.WaitForElement("TopTabPage3");
-	// 	App.Tap("Main 2");
-	// 	App.WaitForElement("TopTabPage2");
-	// 	App.Tap("Main 1");
-
-	// 	App.TapCoordinates(location.Rect.CenterX, location.Rect.CenterY);
-	// 	App.WaitForElement("TopTabPage3");
-	// 	App.Tap("Main 2");
-	// 	App.WaitForElement("TopTabPage2");
-	// 	App.Tap("Main 1");
-	// 	App.TapCoordinates(location.Rect.CenterX, location.Rect.CenterY);
-	// 	App.WaitForElement("TopTabPage3");
-	// }
+	void TopTab(string tab)
+	{
+#if ANDROID
+		App.Tap(Top3);
+#else
+	#if WINDOWS
+        App.WaitForElement("Main 1");
+		App.Tap("navViewItem");
+	#endif
+        App.Tap(Top3);
+#endif
+	}
 }
--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue8741.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue8741.cs
@@ -1,54 +1,54 @@
 ﻿using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
-
+ 
 namespace Microsoft.Maui.TestCases.Tests.Issues;
-
+ 
 public class Issue8741 : _IssuesUITest
 {
 	public Issue8741(TestDevice testDevice) : base(testDevice)
 	{
 	}
-
+ 
 	public override string Issue => "[Bug] [Shell] [Android] ToolbarItem Enabled/Disabled behavior does not work for Shell apps";
+ 
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void Issue8741Test()
+	{
+		App.WaitForElement("Add");
+		App.Tap("Add");
 
-	//[Test]
-	//[Category(UITestCategories.Shell)]
-	//[FailsOnAndroidWhenRunningOnXamarinUITest]
-	//public void Issue8741Test()
-	//{
-	//	App.WaitForElement("Add");
-	//	App.Tap("Add");
+		//var toolbarItemColorValue = GetToolbarItemColorValue();
+		//int disabledAlpha = GetAlphaValue(toolbarItemColorValue);
+ 
+		Assert.That(App.WaitForElement("ClickCount").ReadText(), Is.EqualTo("0"));
+ 
+		App.Tap("ToggleEnabled");
+		App.Tap("Add");
+ 
+		//toolbarItemColorValue = GetToolbarItemColorValue();
+		//int enabledAlpha = GetAlphaValue(toolbarItemColorValue);
+		//Assert.That(disabledAlpha, Is.LessThan(enabledAlpha));
+ 
+		Assert.That(App.WaitForElement("ClickCount").ReadText(), Is.EqualTo("1"));
+ 
+		App.Tap("ToggleEnabled");
+		App.Tap("Add");
+ 
+		Assert.That(App.WaitForElement("ClickCount").ReadText(), Is.EqualTo("1"));
+	}
 
-	//	var toolbarItemColorValue = GetToolbarItemColorValue();
-	//	int disabledAlpha = GetAlphaValue(toolbarItemColorValue);
-
-	//	Assert.AreEqual("0", App.WaitForElement("ClickCount")[0].ReadText());
-
-	//	App.Tap("ToggleEnabled");
-	//	App.Tap("Add");
-
-	//	toolbarItemColorValue = GetToolbarItemColorValue();
-	//	int enabledAlpha = GetAlphaValue(toolbarItemColorValue);
-	//	Assert.Less(disabledAlpha, enabledAlpha);
-
-	//	Assert.AreEqual("1", App.WaitForElement("ClickCount")[0].ReadText());
-
-	//	App.Tap("ToggleEnabled");
-	//	App.Tap("Add");
-
-	//	Assert.AreEqual("1", App.WaitForElement("ClickCount")[0].ReadText());
-	//}
-
-	//private object GetToolbarItemColorValue()
-	//{
-	//	return App.Query(x => x.Text("Add").Invoke("getCurrentTextColor"))[0];
-	//}
-
-	//private int GetAlphaValue(object toolbarItemColorValue)
-	//{
-	//	int color = Convert.ToInt32(toolbarItemColorValue);
-	//	int a = (color >> 24) & 0xff;
-	//	return a;
-	//}
+	//Note: these methods were commented because Appium doesn't provide a reliable way to access the exact color values of UI elements across different platforms and devices.
+	/*private object GetToolbarItemColorValue()
+	{
+		return App.Query(x => x.Text("Add").Invoke("getCurrentTextColor"))[0];
+	}
+ 
+	private int GetAlphaValue(object toolbarItemColorValue)
+	{
+		int color = Convert.ToInt32(toolbarItemColorValue);
+		int a = (color >> 24) & 0xff;
+		return a;
+	}*/
 }
