f6eb2ff290c1f6d8cb4d427c1b657d851532d964
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2024-12-09 05:49:44 -0800

    Migrated the Issues 2499 and 2948

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue2499.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue2499.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_IOS && TEST_FAILS_ON_CATALYST //The test fails on iOS and macOS with a TimeoutException in WaitForElement("cat").
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -12,27 +13,25 @@ public class Issue2499 : _IssuesUITest
 
 	public override string Issue => "Binding Context set to Null in Picker";
 
-	// [Test]
-	// [Category(UITestCategories.Picker)]
-	// [FailsOnIOSWhenRunningOnXamarinUITest]
-	// 	public void Issue2499Test()
-	// 	{
-	// 		App.WaitForElement("picker");
-	// 		App.Tap("picker");
-	// 		App.WaitForElement("cat");
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void Issue2499Test()
+	{
+		App.WaitForElement("picker");
+		App.Tap("picker");
+		App.WaitForElement("cat");
+		App.WaitForElement("mouse");
+		App.Tap("mouse");
 
-	// 		AppResult[] items = App.Query("cat");
-	// 		Assert.AreNotEqual(items.Length, 0);
-	// 		App.WaitForElement(q => q.Marked("mouse"));
-	// 		App.Tap("mouse");
 	// #if __IOS__
 	// 		System.Threading.Tasks.Task.Delay(500).Wait();
 	// 		var cancelButtonText = "Done";
-	// 		App.WaitForElement(q => q.Marked(cancelButtonText));
-	// 		App.Tap(q => q.Marked(cancelButtonText));
+	// 		App.WaitForElement(cancelButtonText);
+	// 		App.Tap(cancelButtonText);
 	// 		System.Threading.Tasks.Task.Delay(1000).Wait();
 	// #endif
-	// 		items = App.Query("cat");
-	// 		Assert.AreEqual(items.Length, 0);
-	// 	}
+
+		App.WaitForNoElement("cat");
+	}
 }
+#endif
