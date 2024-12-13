32dced390812a42d9be710729a7cfc2ec4ca95c3
NafeelaNazhir <nafeela.nazhirhussain@syncfusion.com>
2024-12-13 20:13:26 +0530

    Migrated Issue ShellFlyoutHeaderBehavior and Issue 10134

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue10134.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/Issue10134.cs
@@ -1,7 +1,9 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_ANDROID && TEST_FAILS_ON_CATALYST && TEST_FAILS_ON_WINDOWS // DragCoordinates is not supported on MacCatalyst; in Windows Tab12 is visible, while on Android tapping elements causes the top tabs to scroll back to the beginning.
+using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using UITest.Appium;
 using UITest.Core;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Microsoft.Maui.TestCases.Tests.Issues;
 
@@ -13,46 +15,50 @@ public class Github10134 : _IssuesUITest
 
 	public override string Issue => "Shell Top Tabbar focus issue";
 
-	// [Test]
-	// [Category(UITestCategories.Shell)]
-	// [FailsOnIOS]
-	// public void TopTabsDontScrollBackToStartWhenSelected() 
-	// {
-	// 	var element1 = App.WaitForElement("Tab 1", "Shell hasn't loaded")[0].Rect;
-	// 	App.WaitForNoElement("Tab 12", "Tab shouldn't be visible");
+	[Test]
+	[Category(UITestCategories.Shell)]
 
-	// 	Xamarin.UITest.Queries.AppRect element2 = element1;
+	public void TopTabsDontScrollBackToStartWhenSelected()
+	{
+		App.WaitForElement("Tab 1");
+		var element1 = App.WaitForElement("Tab 1").GetRect();
+		App.WaitForNoElement("Tab 12");
 
-	// 	for (int i = 2; i < 20; i++)
-	// 	{
-	// 		var results = App.Query($"Tab {i}");
+		var element2 = element1;
 
-	// 		if (results.Length == 0)
-	// 			break;
+		for (int i = 2; i < 20; i++)
+		{
+			var results = App.FindElements($"Tab {i}");
 
-	// 		element2 = results[0].Rect;
-	// 	}
+			foreach (var result in results)
+			{
+				element2 = result.GetRect();
+				break;
+			}
 
-	// 	App.DragCoordinates(element2.CenterX, element2.CenterY, element1.CenterX, element1.CenterY);
+			if (results.Count == 0)
+				break;
+		}
+		
+		App.DragCoordinates(element2.CenterX(), element2.CenterY(), element1.CenterX(), element1.CenterY());
 
-	// 	App.WaitForNoElement("Tab 1");
-	// 	bool testPassed = false;
+		App.WaitForNoElement("Tab 1");
+		bool testPassed = false;
+		for (int i = 20; i > 1; i--)
+		{
+			var results = App.FindElements($"Tab {i}");
 
-	// 	// figure out what tabs are visible
-	// 	for (int i = 20; i > 1; i--)
-	// 	{
-	// 		var results = App.Query($"Tab {i}");
+			if (results.Count > 0)
+			{
+				App.Tap($"Tab {i}");
+				App.WaitForElement($"Tab {i}");
+				testPassed = true;
+				break;
+			}
+		}
+		App.WaitForNoElement("Tab 1");
+		Assert.That(testPassed, Is.True);
 
-	// 		if (results.Length > 0)
-	// 		{
-	// 			App.Tap($"Tab {i}");
-	// 			App.WaitForElement($"Tab {i}");
-	// 			testPassed = true;
-	// 			break;
-	// 		}
-	// 	}
-
-	// 	App.WaitForNoElement("Tab 1");
-	// 	Assert.IsTrue(testPassed);
-	// }
+	}
 }
+#endif
--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/ShellFlyoutHeaderBehavior.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/XFIssue/ShellFlyoutHeaderBehavior.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿#if TEST_FAILS_ON_CATALYST // Scroll not supported on MacCatalyst
+using NUnit.Framework;
 using UITest.Appium;
 using UITest.Core;
 
@@ -11,18 +12,67 @@ public class ShellFlyoutHeaderBehavior : _IssuesUITest
 	}
 
 	public override string Issue => "Shell Flyout Header Behavior";
+#if !WINDOWS // AuomationId not working in Windows
+	[Test]
+	[Category(UITestCategories.Shell)]
+	public void FlyoutHeaderBehaviorFixed()
+	{
+		App.WaitForElement("Fixed");
+		App.Tap("Fixed");
+		float startingHeight = GetFlyoutHeight();
+		App.ScrollDown("Item 4", ScrollStrategy.Gesture);
+		float endHeight = GetFlyoutHeight();
 
-	//[Test]
-	//[Category(UITestCategories.Shell)]
-	//public void FlyoutContentTests()
-	//{
-	//	App.WaitForElement("PageLoaded");
-	//	TapInFlyout("Flyout Item");
-	//	App.Tap("ToggleContent");
-	//	TapInFlyout("ContentView");
-	//	TapInFlyout("Flyout Item");
-	//	App.Tap("ToggleFlyoutContentTemplate");
-	//	TapInFlyout("ContentView");
-	//	TapInFlyout("Flyout Item");
-	//}
+		Assert.That(startingHeight, Is.EqualTo(endHeight));
+	}
+#endif
+#if !WINDOWS && !IOS // Rect value differs on IOS
+    [Test]
+    [Category(UITestCategories.Shell)]
+    public void FlyoutHeaderBehaviorCollapseOnScroll()
+    {
+        App.WaitForElement("CollapseOnScroll");
+        App.Tap("CollapseOnScroll");
+        float startingHeight = GetFlyoutHeight();
+        App.ScrollDown("Item 4", ScrollStrategy.Gesture);
+        float endHeight = GetFlyoutHeight();
+ 
+        Assert.That(startingHeight, Is.GreaterThan(endHeight));
+    }
+#endif
+#if !IOS && !ANDROID // Rect value differs on IOS && Android
+    [Test]
+    [Category(UITestCategories.Shell)]
+    public void FlyoutHeaderBehaviorScroll()
+    {
+        App.WaitForElement("Scroll");
+        App.Tap("Scroll");
+        var startingY = GetFlyoutY();
+        App.ScrollDown("Item 5", ScrollStrategy.Gesture);
+        var nextY = GetFlyoutY();
+ 
+        while (nextY != null && startingY != null)
+        {
+            Assert.That(startingY.Value, Is.GreaterThan(nextY.Value));
+            startingY = nextY;
+            App.ScrollDown("Item 5", ScrollStrategy.Gesture);
+            nextY = GetFlyoutY();
+        }
+    }
+#endif
+	float GetFlyoutHeight() =>
+		App.WaitForElement("FlyoutHeaderId").GetRect().Height;
+
+	float? GetFlyoutY()
+	{
+		var flyoutHeader =
+			App.FindElements("FlyoutHeaderId");
+
+		foreach (var element in flyoutHeader)
+		{
+			return element.GetRect().Y;
+		}
+		return null;
+	}
 }
+#endif
