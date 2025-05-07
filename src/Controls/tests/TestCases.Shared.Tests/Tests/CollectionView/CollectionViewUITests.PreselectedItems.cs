11a8637ec68fe9060dea764dd6c65b01662a2491
LogishaSelvarajSF4525 <logisha.selvaraj@syncfusion.com>
2025-05-07 19:36:12 +0530

    added test cases for collectionviewgallery selection

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/CollectionView/CollectionViewUITests.PreselectedItems.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests
+{
+	public class CollectionViewPreselectedItemsUITests : CollectionViewUITests
+	{
+		public CollectionViewPreselectedItemsUITests(TestDevice device)
+			: base(device)
+		{
+		}
+
+		[Test]
+		[Category(UITestCategories.CollectionView)]
+		public void PreselectedItems()
+		{
+			// Navigate to the selection galleries
+			VisitInitialGallery("Selection");
+
+			// Navigate to the specific sample inside selection galleries
+			VisitSubGallery("Preselected Items");
+
+			VerifyScreenshot();
+		}
+	}
+}
