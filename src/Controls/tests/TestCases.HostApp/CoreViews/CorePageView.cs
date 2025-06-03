7db334d01dc829299ab694e32032259efa87eee3
nivetha-nagalingam <nivetha.nagalingam@syncfusion.com>
2025-06-03 16:28:58 +0530

    Added sample for GraphicsView

--- src/Controls/tests/TestCases.HostApp/CoreViews/CorePageView.cs
+++ src/Controls/tests/TestCases.HostApp/CoreViews/CorePageView.cs
@@ -1,5 +1,6 @@
 ﻿using Controls.Sample.UITests;
 using Maui.Controls.Sample.CollectionViewGalleries;
+using Maui.Controls.Sample;
 
 namespace Maui.Controls.Sample
 {
@@ -78,6 +79,7 @@ namespace Maui.Controls.Sample
 			new GalleryPageFactory(() => new WebViewCoreGalleryPage(), "WebView Gallery"),
 			new GalleryPageFactory(() => new SliderControlPage(), "Slider Feature Matrix"),
 			new GalleryPageFactory(() => new CollectionViewFeaturePage(), "CollectionView Feature Matrix"),
+			new GalleryPageFactory(() => new GraphicsViewControlPage(), "GraphicsView Feature Matrix"),
 		};
 
 		public CorePageView(Page rootPage)
